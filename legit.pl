a16b1b4b0612ea25491bd9761f6c1273a62e18f7
AMeikle99 <aidenmeikle@ymail.com>
2018-09-20 01:45:22 +1000

    Added functionality of addLegit

--- legit.pl
+++ legit.pl
@@ -3,6 +3,8 @@
 use strict;
 use warnings;
 use Cwd;
+use File::Copy;
+use File::Compare;
 
 
 #Global Variables that can be accessed by all routines
@@ -10,12 +12,17 @@ use Cwd;
 our $ROOT_FOLDER = ".legit";
 our $LOGS_FOLDER = "logs";
 our $SNAPSHOT_FOLDER = "snapshots";
+our $INDEX_FOLDER = "index";
 our $REFS_FOLDER = "refs";
 our $HEADS_FOLDER = "refs/heads";
 
 our $SNAPSHOT_FILE = ".S";
 
 our $WORKING_DIRECTORY = getcwd();
+our $CURRENT_BRANCH;
+our $CURRENT_SNAPSHOT;
+
+our $MAX_COMMIT;
 
 ######################
 
@@ -33,8 +40,13 @@ sub main{
 
 	if($args[0] eq "init"){
 		initLegit(@args);
+	}elsif($args[0] eq "add"){
+		shift @args;
+		addLegit(@args);
 	}
 
+
+	changedFiles();
 
 	exit(0);
 
@@ -46,17 +58,37 @@ sub validateArguments{
 	my @args = @_;
 	my $command;
 
+	chdir($WORKING_DIRECTORY);
+
 	#Check the supplied arguments are valid and sufficient parameters provided
 	if($#args >= 0){
 		#Check that the 'init' command has no extra arguments passed in
 		if($args[0] eq "init"){
 			if($#args >0){
-				printf "Usage: $0 init\n";
+				printf STDERR "Usage: $0 init\n";
 				exit(1);
 			}
 		#The supplied command cannot be found, print error and exit
+		}elsif($args[0] eq "add"){
+			if($#args < 1){
+				printf STDERR "legit.pl: error: internal error Nothing specified, nothing added.\n";
+				exit(1);
+			}else{
+				foreach my $file(@args[1..$#args]){
+					if($file =~ /^[\.|\/]/){
+						printf STDERR "legit.pl: error: invalid filename '$file'\n";
+						exit(1);
+					}elsif($file =~ /^-/){
+						printf STDERR "Usage: legit.pl <filenames>\n";
+						exit(1);
+					}elsif(!(-f $file)){
+						printf STDERR "legit.pl: error: can not open '$file'\n";
+						exit(1);
+					}
+				}
+			}
 		}else{
-			print "legit.pl: error: unknown command $args[0]\n";
+			print STDERR "legit.pl: error: unknown command $args[0]\n";
 			displayOptionsList();
 			exit(1);
 		}
@@ -86,17 +118,18 @@ sub displayOptionsList{
 
 }
 
+#Deals with initialising the .legit folder (if it doesn't already exist)
 sub initLegit{
 
 	#Check if the .legit folder already exists
 	#Exits with error message if it does
 	if(-d $ROOT_FOLDER){
-		printf "legit.pl: error: .legit already exists\n";
+		printf STDERR "legit.pl: error: .legit already exists\n";
 		exit(1);
 	}
 
 	#Create the .legit folder
-	mkdir($ROOT_FOLDER) or die "Unable to create .legit folder";
+	mkdir($ROOT_FOLDER);
 
 	#Create all the subdirectories
 	chdir($ROOT_FOLDER);
@@ -104,6 +137,7 @@ sub initLegit{
 	mkdir($SNAPSHOT_FOLDER);
 	mkdir($REFS_FOLDER);
 	mkdir($HEADS_FOLDER);
+	mkdir($INDEX_FOLDER);
 
 	mkdir("$LOGS_FOLDER/$REFS_FOLDER");
 	mkdir("$LOGS_FOLDER/$HEADS_FOLDER");
@@ -112,5 +146,29 @@ sub initLegit{
 	#Print success message
 	printf "Initialized empty legit repository in .legit\n";
 
+}
 
+sub changedFiles{
+	if(compare("10.txt", ".legit/index/10.txt")==0){
+		printf "The files are equal\n";
+	}else{
+		printf "The files aren't equal\n";
+	}
+}
+
+#Deals with adding a copy of the files to the temporary index folder
+sub addLegit{
+	my @files = @_;
+
+	#Checks that the .legit folder is actually present
+	if(!(-d ".legit")){
+		printf STDERR "legit.pl: error: no .legit directory containing legit repository exists\n";
+		exit(1);
+	}
+
+	#Copies each of the listed files into the index folder
+	foreach my $file(@files){
+		printf "Adding $file to index...\n";
+		copy($file, ".legit/index/$file");
+	}
 }
