903664cf1dee055e8a3170a5afead3977ef5903d
AMeikle99 <aidenmeikle@ymail.com>
2018-10-04 00:59:01 +1000

    twek

--- legit.pl
+++ legit.pl
@@ -523,7 +523,10 @@ sub rmLegit{
 		if($isForce == 0){
 			#Check each file in CWD, index and snapshot are the same so data wouldn't be lost
 			foreach my $file(@files){
-				if(compare("$ROOT_FOLDER/$INDEX_FOLDER/$file", "$ROOT_FOLDER/$SNAPSHOT_FOLDER/$CURRENT_SNAPSHOT/$file") != 0 && (compare($file, "$ROOT_FOLDER/$INDEX_FOLDER/$file") != 0)){
+				if(!(-e "$ROOT_FOLDER/$INDEX_FOLDER/$file")){
+					printf STDERR "legit.pl: error: '$file' is not in the legit repository\n";
+					exit(1);
+				}elsif(compare("$ROOT_FOLDER/$INDEX_FOLDER/$file", "$ROOT_FOLDER/$SNAPSHOT_FOLDER/$CURRENT_SNAPSHOT/$file") != 0 && (compare($file, "$ROOT_FOLDER/$INDEX_FOLDER/$file") != 0)){
 					printf STDERR "legit.pl: error: '$file' in index is different to both working file and repository\n";
 					exit(1);
 				}
